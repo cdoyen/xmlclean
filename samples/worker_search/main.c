e830337b8c4ffa641d4f5ab6ba463cb79f0698ad
cdoyen <postme33@arcor.de>
2016-11-05 11:07:11 +0100

    initial

---
+++ samples/worker_search/main.c
@@ -0,0 +1,58 @@
+#include "xmlclean.h"
+#include <stdio.h> /* fopen,fclose,fprintf,stderr */
+
+int worker_search(int typ, const unsigned char *tag, size_t taglen, int out(), void* f, Parser *p)
+{
+	switch (typ)
+	{
+	case NORMALCLOSE_:
+	{
+		const Memblock *pattern = p->userdata;
+		if (anymatch(pattern->b, pattern->z, tag + 1, taglen - 1))  /* führendes '/' beim closetag ignorieren mit +1/-1 */
+		{
+			out(f, p->content, p->contentlen);
+		}
+	}
+	break;
+	case OPENTAG_:
+		break;
+	case SELFCLOSE_:
+		break;
+	case FRAMECLOSE_:
+		break;
+	case COMMENT_:
+		break;
+	case PROLOG_:
+		break;
+	case UNKNOWN_:
+		break;
+	}
+	return XML_OK;
+}
+
+int main(int argc, char**argv)
+{
+	FILE *f = fopen(argv[1], "rb");
+	if (!f) return perror(argv[1]), 1;
+	{
+		Parser p = { f, 0, worker_search, 0, writeln, &(Memblock) { 20, "name|*capital*|*tude" } };
+		{
+			int r = parse(&p);
+			if (r)
+			{
+				if (r == ERRMEM)
+					fprintf(stderr, "Fehler Speichermangel");
+				else
+					if (r == ERRHIERAR)
+						fprintf(stderr, "Fehler Hierarchie");
+					else
+						fprintf(stderr, "Fehler Output");
+			}
+
+			fclose(f);
+
+			done(&p);
+			return r;
+		}
+	}
+}
